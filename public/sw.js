aa5eabc4579bf19aad653c6f6f03ac6a9f019044
guerrasur <lucioguerrasur@gmail.com>
2026-09-22 10:55:20 -0300

    Refresh service worker cache for 0.34.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pelaobolao-shell-0.33.0';
+const CACHE = 'pelaobolao-shell-0.34.0';
 const CORE = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon.svg'];
 
 self.addEventListener('install', event => {
